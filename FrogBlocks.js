5b6e9ad46818ce5241f09bbf3441cd7585797c81
daoistcoder <deservekindness@gmail.com>
2023-06-26 22:53:33 +0800

    add FrogBlock.js

---
+++ FrogBlocks.js
@@ -0,0 +1,15 @@
+function solution(blocks) {
+  let maxDistance = 0;
+
+  for (let i = 0; i < blocks.length - 1; i++) {
+    for (let j = i + 1; j < blocks.length; j++) {
+      if (blocks[j] >= blocks[i]) {
+        let distance = j - i;
+        if (distance > maxDistance) {
+          maxDistance = distance;
+        }
+      }
+    }
+  }
+  return maxDistance
+}
