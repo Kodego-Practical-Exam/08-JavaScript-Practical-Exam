eb096be74c3a02b077948c444121ee9db081b949
daoistcoder <deservekindness@gmail.com>
2023-07-01 22:00:10 +0800

    update app.js

--- APIJSON Exam - Practical Exam/app.js
+++ APIJSON Exam - Practical Exam/app.js
@@ -12,22 +12,70 @@ function createDivAndDisplayData(data) {
   // Get the container element where you want to display the scrollable list
   const container = document.getElementById('container');
 
-  // Create a div element
-  const div = document.createElement('div');
-  div.className = 'scrollable-list';
+  // Create a div element for the loading message
+  const loadingDiv = document.createElement('div');
+  loadingDiv.textContent = 'Loading...';
+
+  // Append the loading div to the container
+  container.appendChild(loadingDiv);
+
+  // Create a div element for the scrollable list
+  const listDiv = document.createElement('div');
+  listDiv.className = 'scrollable-list';
 
   // Loop through the data and create the list items
   for (const key in data) {
     if (data.hasOwnProperty(key)) {
       const item = data[key];
-      const listItem = document.createElement('div');
-      listItem.textContent = `${item.name} (${item.unit}): ${item.value}`;
 
-      // Append the list item to the div
-      div.appendChild(listItem);
+      // Create the list item div
+      const listItem = document.createElement('div');
+      listItem.className = 'list-item';
+
+      // Create the logo div (empty for now)
+      const logoDiv = document.createElement('div');
+      logoDiv.className = 'logo';
+
+      // Create the content div
+      const contentDiv = document.createElement('div');
+      contentDiv.className = 'content';
+
+      // Create the h2 element for the rate
+      const rateH2 = document.createElement('h2');
+      rateH2.textContent = `Rate = ${item.value}`;
+
+      // Create the first paragraph for the crypto name
+      const nameP = document.createElement('p');
+      nameP.textContent = `Crypto name: ${item.name}`;
+
+      // Create the second paragraph for the crypto unit
+      const unitP = document.createElement('p');
+      unitP.textContent = `Crypto unit: ${item.unit}`;
+
+      // Append the elements to the content div
+      contentDiv.appendChild(rateH2);
+      contentDiv.appendChild(nameP);
+      contentDiv.appendChild(unitP);
+
+      // Append the logo div and content div to the list item div
+      listItem.appendChild(logoDiv);
+      listItem.appendChild(contentDiv);
+
+      // Append the list item to the list div
+      listDiv.appendChild(listItem);
     }
   }
 
-  // Append the div to the container
-  container.appendChild(div);
+  // Remove the loading div
+  container.removeChild(loadingDiv);
+
+  // Append the list div to the container
+  container.appendChild(listDiv);
+
+  // Create a div element for the end of list message
+  const endDiv = document.createElement('div');
+  endDiv.textContent = 'End of list';
+
+  // Append the end div to the container
+  container.appendChild(endDiv);
 }
