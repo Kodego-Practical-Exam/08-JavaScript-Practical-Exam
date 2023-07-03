142619b0a7737ca21d58c26132dd479d237af038
daoistcoder <deservekindness@gmail.com>
2023-07-03 21:12:42 +0800

    tik tak toe

--- tic-tac-toe-demo/src/app/page.tsx
+++ tic-tac-toe-demo/src/app/page.tsx
@@ -1,71 +1,11 @@
 "use client"; // This is a client component 👈🏽
+
 import React, { useState } from 'react';
-
-type SquareProps = {
-  value: string | null;
-  onSquareClick: () => void;
-};
-
-function Square({ value, onSquareClick }: SquareProps) {
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
-}
-
-type BoardProps = {
-  xIsNext: boolean;
-  squares: Array<string | null>;
-  onPlay: (nextSquares: Array<string | null>) => void;
-};
-
-function Board({ xIsNext, squares, onPlay }: BoardProps) {
-  function handleClick(i: number) {
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = 'X';
-    } else {
-      nextSquares[i] = 'O';
-    }
-    onPlay(nextSquares);
-  }
-
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = 'Winner: ' + winner;
-  } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-  }
-
-  return (
-    <>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-    </>
-  );
-}
+import Board from './board';
 
 export default function Game() {
   const [currentSquares, setCurrentSquares] = useState<Array<string | null>>(Array(9).fill(null));
-  const xIsNext = currentSquares.filter((s) => s === null).length % 2 === 0;
+  const oIsNext = currentSquares.filter((s) => s === null).length % 2 === 0;
 
   function handlePlay(nextSquares: Array<string | null>) {
     setCurrentSquares(nextSquares);
@@ -82,14 +22,14 @@ export default function Game() {
   } else if (currentSquares.every((s) => s !== null)) {
     status = 'Draw';
   } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    status = 'Next player: ' + (oIsNext ? 'O' : 'X');
   }
 
   return (
     <div className="game">
       <h1>Tic Tac Toe</h1>
       <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        <Board xIsNext={!oIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="status">{status}</div>
       <button className="restart-button" onClick={restartGame}>
@@ -118,4 +58,3 @@ function calculateWinner(squares: Array<string | null>) {
   }
   return null;
 }
-
