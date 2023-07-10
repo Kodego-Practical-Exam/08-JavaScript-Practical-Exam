1eecf0b5333c3f3434c5ba438f92cf1c4ff43af2
daoistcoder <deservekindness@gmail.com>
2023-07-10 23:57:10 +0800

    resolved string render issue

--- tik-tak-toe-react-node-app/client/src/App.tsx
+++ tik-tak-toe-react-node-app/client/src/App.tsx
@@ -65,7 +65,7 @@ const App: React.FC = () => {
 
   const renderSquare = (index: number) => {
     console.log(`Squarevalue${index}:`, squares[index]);
-    const squareValue = squares[index];
+    const squareValue = squares[index] || "";
 
 
 
